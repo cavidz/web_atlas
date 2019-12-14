3fd9f04394646b871aa43a24fdbd814373c96c38
Javid Ahadov <cavidz@gmail.com>
2019-12-14 17:46:54 +0300

    Added new files and modifed some old files

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,19 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/style.css',
+        'https://fonts.googleapis.com/css?family=Roboto%3A700%7COpen+Sans%3A400%2C700&#038;subset=latin',
+        'css/featherlight.css',
     ];
     public $js = [
+        'js/modernizr.custom.24530.js',
+        'js/picturefill.min.js',
+        'js/underscore.min.js?ver=1.6.0',
+        'js/featherlight.js',
+        'js/main.min.js?ver=1.1.0-3-g4c75ad2',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
+        //'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
 }
