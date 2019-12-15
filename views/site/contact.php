483710249fa6932db710585b6106a0b42dd72304
Javid Ahadov <cavidz@gmail.com>
2019-12-16 02:54:21 +0300

    Contact page modification

--- views/site/contact.php
+++ views/site/contact.php
@@ -21,8 +21,286 @@
 </div><!-- /.breadcrumbs -->
 
 <section class="map margin-bottom-60">
-    <div id="map-canvas" data-lat="40.4302890" data-lng="49.8781440" data-zoom="15"></div>
+    <div id="map-canvas" data-lat="40.4302890" data-lng="49.8781440" data-zoom="17"></div>
     <div class="addresses-block">
         <a data-lat="40.4302890" data-lng="49.8781440" data-string="Atlas Engineering"></a>
     </div>
 </section>
+
+<div class="container">
+            
+    <div class="row margin-bottom-60">
+        
+        <div class="col-lg-4">
+            
+            <div class="card">
+                <img src="images/contact-logo.png" alt="Picture of ">
+                <div class="card-block contact-profile__container">
+                    <div class="contact-profile__social-icons">
+                        <a class="contact-profile__social-icon" href="https://www.facebook.com/AtlasEngineering/" target="_self">
+                            <i class="fa fa-facebook-square"></i>
+                        </a>
+                        <a class="contact-profile__social-icon" href="https://twitter.com/atlasengineering" target="_self">
+                            <i class="fa fa-twitter-square"></i>
+                        </a>
+                        <a class="contact-profile__social-icon" href="https://www.youtube.com/user/AtlasEngineering" target="_self">
+                            <i class="fa fa-youtube-square"></i>
+                        </a>
+                    </div><!-- /.contact-profile__social-icons -->
+                    <div class="contact-profile__items">
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-map-marker"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                227 Marion Street Avenue <br> Columbia SC 29201 <br> United Kingdom
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                1-888-123-4567
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="345d5a525b744740464157404146511a575b59">[email&#160;protected]</a>
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-compass"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                www.structure.com
+                            </p>
+                        </div>
+                    </div><!-- /.contact-profile__items -->
+                </div><!-- /.card-block -->
+            </div><!-- /.card -->
+            
+        </div><!-- /.col -->
+        
+        <div class="col-lg-8">
+            
+            <div role="form" class="contact">
+                
+                <form data-toggle="validator" method="post" action="form.php" class="contact-form aSubmit">
+                    <div style="display:none"><input type="text" name="maximus" value=""></div>
+                    <input type="hidden" name="theSubject" value="StructurePress Contact Request">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <span class="contact-form-control-wrap name">
+                                <input name="name" value="" size="40" class="contact-form-control contact-text contact-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Name *" type="text" required>
+                            </span>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <span class="contact-form-control-wrap surname">
+                                <input name="surname" value="" size="40" class="contact-form-control contact-text contact-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Surname *" type="text" required>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <span class="contact-form-control-wrap email">
+                                <input name="email" value="" size="40" class="contact-form-control contact-text contact-email contact-validates-as-required contact-validates-as-email form-control" aria-required="true" aria-invalid="false" placeholder="E-mail *" type="email" required>
+                            </span>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <span class="contact-form-control-wrap service">
+                                <input name="service" value="" size="40" class="contact-form-control contact-text form-control" aria-invalid="false" placeholder="Service" type="text">
+                            </span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <span class="contact-form-control-wrap your-message">
+                                <textarea name="your-message" cols="40" rows="10" class="contact-form-control contact-textarea form-control" aria-invalid="false" placeholder="Your Message *" required></textarea>
+                            </span>
+                            <p></p>
+                            <div class="contact-notice">All fields with * are obligatory</div>
+                            <p>
+                                <input value="SEND MESSAGE" class="contact-form-control contact-submit btn btn-primary" type="submit">
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="response success">Your message was sent; we'll be in touch shortly!</div>
+                    <div class="response error">Unfortunately, we could not sent your message right now. Please try again.</div>
+                
+                </form>
+            
+            </div>
+            
+        </div><!-- /.col -->
+        
+    </div><!-- /.row -->
+    
+    <div class="row margin-bottom-100">
+        
+        <div class="col-md-3">
+            
+            <div class="card">
+                <img src="images/14.jpg" alt="Picture of Baku Branch">
+                <div class="card-block contact-profile__container">
+                    <div class="contact-profile__items">
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-map-marker"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                227 Marion Street Avenue <br> Columbia SC 29201 <br> United Kingdom
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                1-888-123-4567
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="50393e363f102324222533242522357e333f3d">[email&#160;protected]</a>
+                            </p>
+                        </div>
+                    </div><!-- /.contact-profile__items -->
+                    <div>
+                        <span class="contact-profile__name">Baku Branch</span>
+                    </div>
+                </div><!-- /.card-block -->
+            </div><!-- /.card -->
+            
+        </div><!-- /.col -->
+        
+        <div class="col-md-3">
+            
+            <div class="card">
+                <img src="images/68.jpg" alt="Picture of Istanbul Branch">
+                <div class="card-block contact-profile__container">
+                    <div class="contact-profile__items">
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-map-marker"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                227 Marion Street Avenue <br> Columbia SC 29201 <br> United Kingdom
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                1-888-123-4567
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="553c3b333a152621272036212027307b363a38">[email&#160;protected]</a>
+                            </p>
+                        </div>
+                    </div><!-- /.contact-profile__items -->
+                    <div>
+                        <span class="contact-profile__name">Istanbul Branch</span>
+                    </div>
+                </div><!-- /.card-block -->
+            </div><!-- /.card -->
+            
+        </div><!-- /.col -->
+        
+        <div class="col-md-3">
+            
+            <div class="card">
+                <img src="images/62.jpg" alt="Picture of New York Branch">
+                <div class="card-block contact-profile__container">
+                    <div class="contact-profile__items">
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-map-marker"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                227 Marion Street Avenue <br> Columbia SC 29201 <br> United Kingdom
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                1-888-123-4567
+                            </p>
+                        </div>
+                        <div class="contact-profile__item">
+                            <div class="contact-profile__icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <p class="contact-profile__text">
+                                <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="1b72757d745b686f696e786f6e697e35787476">[email&#160;protected]</a>
+                            </p>
+                        </div>
+                    </div><!-- /.contact-profile__items -->
+                    <div>
+                        <span class="contact-profile__name">New York Branch</span>
+                    </div>
+                </div><!-- /.card-block -->
+            </div><!-- /.card -->
+            
+        </div><!-- /.col -->
+        
+        <div class="col-md-3">
+            
+            <div class="time-table">
+                <h3><span class="icon icons-ornament-left"></span>
+                    Opening Time
+                    <span class="icon icons-ornament-right"></span>
+                </h3>
+                <div class="inner-bg">
+                    <dl class="week-day ">
+                        <dt>Monday</dt>
+                        <dd>8:00-16:00</dd>
+                    </dl>
+                    <dl class="week-day light-bg">
+                        <dt>Tuesday</dt>
+                        <dd>8:00-16:00</dd>
+                    </dl>
+                    <dl class="week-day">
+                        <dt>Wednesday</dt>
+                        <dd>8:00-16:00</dd>
+                    </dl>
+                    <dl class="week-day light-bg">
+                        <dt>Thursday</dt>
+                        <dd>8:00-16:00</dd>
+                    </dl>
+                    <dl class="week-day">
+                        <dt>Friday</dt>
+                        <dd>8:00-16:00</dd>
+                    </dl>
+                    <dl class="week-day light-bg">
+                        <dt>Saturday</dt>
+                        <dd>8:00-16:00</dd>
+                    </dl>
+                    <dl class="week-day  today">
+                        <dt>Sunday</dt>
+                        <dd>8:00-16:00</dd>
+                    </dl>
+                </div>
+            </div>
+            
+        </div><!-- /.col -->
+        
+    </div><!-- /.row -->
+    
+</div><!-- /.container -->
